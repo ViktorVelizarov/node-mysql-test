82fec5c7793e9d840c7d8e8881ada8d7c10c145b
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-03-09 13:18:40 +0100

    add env file

--- database.js
+++ database.js
@@ -1,12 +1,14 @@
 //here is all the code that connects to mysql and queries the db
 
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'notes_app'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
   }).promise()
 
   export async function getNotes() {
